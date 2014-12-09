4e6e9098a12aa535f3df61366bfdb8df341982c4
nataliaSumali <natalia.sumali@gmail.com>
2014-12-08 20:35:14 -0800

    add Bootstrap to the app and controller and service on app.js

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -12,12 +12,30 @@ angular.module('spaApp', ['ui.router', 'templates'])
 	})
 	.state('about', {
 		url: '/about',
-		templateUrl: 'about.html'
+		templateUrl: 'about.html',
+		controller: 'aboutController'
 	})
 	.state('map', {
 		url: '/map',
 		templateUrl: 'map.html'
 	})
+})
+
+.controller('aboutController', function($scope, api) {
+	api.getPlaces()
+	.then(function(data){
+		$scope.data = data.images
+	})
+})
+
+.service('api', function($http) {
+	return {
+		getPlaces: function() {
+			var promise = $http.get('https://api.instagram.com/v1/media/popular?client_id=43d5ec3d450445fa9aafc9765600598b')
+			.then(function(response) {
+				return response
+			});
+			return promise;
+		}
+	}
 });
-
-
