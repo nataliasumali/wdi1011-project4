538dce58121d04009e215c66a0b9105fe692ac94
nataliaSumali <natalia.sumali@gmail.com>
2014-12-15 09:31:51 -0800

    get multiple images on the app - thanks Toan

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -22,25 +22,27 @@ angular.module('spaApp', ['ui.router', 'templates'])
 })
 
 .controller('homeController', function($scope, api) {
-		
-		api.getPlaces()
-		.then(function(data){
-		console.log(data);
-		$scope.data = data.data.data[0].images.thumbnail
-		console.log($scope.data);
-	}
+
+	api.getPlaces()
+	.then(function(data) {
+		$scope.data = data.data;
+	})
+
 })
 
 .service('api', function($http) {
+
 	return {
-		getPlaces: function(userLat, userLng) {
+
+		getPlaces: function() {
+
 			var promise = $http.get('https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&client_id=43d5ec3d450445fa9aafc9765600598b')
 			.then(function(response) {
 				return response
 			});
+
 			return promise;
+
 		}
 	}
-})
-
-
+});
